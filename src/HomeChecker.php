6e8b7af358bbf8781c25de21887dd1d2aa023c0e
Federico Boncaldo <federico@cryptolab.net>
2020-03-12 13:14:03 +0000

    Create HomeChecker base class

---
+++ src/HomeChecker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+abstract class HomeChecker
+{
+    protected $successor;
+
+    abstract public function check(HomeStatus $home);
+
+    public function succeedWith(HomeChecker $successor)
+    {
+        $this->successor = $successor;
+    }
+
+    public function next(HomeStatus $home)
+    {
+        if ($this->successor) {
+            $this->successor->check($home);
+        }
+    }
+}
